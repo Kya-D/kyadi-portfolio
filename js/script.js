961eed7305d749ce185d8e320b4c03402f7adabb
Kya-D <kyadi1192@gmail.com>
2023-03-01 11:31:23 +0300

    commit finished

--- js/script.js
+++ js/script.js
@@ -1,14 +1,79 @@
+// animation text
+const text = document.querySelector('.fancy'),
+      strText = text.textContent,splitText = strText.split("");
+text.textContent = "";
+// injecte les caractères une à une dans un span
+for (let i=0; i<splitText.length; i++){
+  text.innerHTML += "<span class='spn'>" + splitText[i] + "</span>";
+}
+
+let char = 0,
+    timer = setInterval(onTick, 50);
+// effet d'apparition
+function onTick(){
+  const span = text.getElementsByClassName('spn')[char];
+  span.classList.add('fade');
+  char++
+  if (char === splitText.length){
+    complete();
+    return;
+  }
+}
+
+function complete(){
+  clearInterval(timer);
+  timer = null;
+}
+
+
 // animation machine à écrire
 
-const txtAnim = document.getElementById('txtType');
+/* let j = 0;
+let txt = 'Graphic & Web Designer';
+let speed = 100;
 
-new Typewriter(txtAnim, {
-  loop: true,
-})
-.pauseFor(1000)
-.typeString('<span style="font-size:1.5rem;color:#FECB00;font-weight:600;">Graphic Designer</span>')
-.pauseFor(1000)
-.deleteChars(17)
-.typeString('<span style="font-size:1.5rem;color:#FECB00;font-weight:600;">Web Designer</span>')
-.pauseFor(1000)
-.start()
+function typeWriter() {
+  if (j < txt.length) {
+    document.getElementById("typeWriter").innerHTML += txt.charAt(j);
+    j++;
+    setTimeout(typeWriter, speed);
+  }
+} */
+
+function sleep(ms){
+  return new Promise ((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = ["Web Designer", "Graphic Designer"],
+      el = document.getElementById("typeWriter");
+
+let sleepTime = 100,
+    curPhraseIndex = 0;
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+
+    for (let i = 0; i<curWord.length; i++){
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 20);
+
+    for (let i = curWord.length; i>0; i--){
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    if (curPhraseIndex === phrases.length - 1){
+      curPhraseIndex = 0;
+    } else{
+      curPhraseIndex++;
+    }
+
+  }
+}
+
+writeLoop();
